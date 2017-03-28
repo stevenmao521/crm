a52852a5ee4c383408b7b6281498ad4af6b0df58
maozi <644961289@qq.com>
2017-03-28 15:36:51 +0800

    .

---
+++ App/Common/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    //设置可访问目录
+    'MODULE_ALLOW_LIST' => array('Home'),
+    //设置默认目录
+    'DEFAULT_MODULE' => 'Home',
+    //URL重写模式
+    'URL_MODEL' => 2,
+    //设置模版后缀
+    'TMPL_TEMPLATE_SUFFIX' => '.html',
+    //设置默认主题目录
+    'DEFAULT_THEME' => 'Default',
+);
